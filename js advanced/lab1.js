c78fadc80cab91246ff5e3ee3e26fcdc0936ffbb
Mohand Ayman Ahmed <mohandayman0127@gmail.com>
2023-03-02 14:48:21 +0200

    kab 1

---
+++ js advanced/lab1.js
@@ -0,0 +1,44 @@
+var rectangle =  {
+    width:300 ,
+    hight:200, 
+    area: function(){
+        return this.width*this.hight
+    },
+    perimeter:function(){return (this.width+this.hight)*2},
+    displayinfo:function(){return `width = ${this.width} , hight ${this.hight} , area = ${this.area()} , perimeter = ${this.perimeter()} `}
+}
+
+
+console.log(rectangle.displayinfo())
+
+
+// let new = function ( obj) {
+//     for(property in obj){
+//         if(typeof obj[property] != 'function')
+// { 
+// Object.defineProperty(obj , `set${property}`,function(val){obj.property  = val})
+// Object.defineProperty(obj , `get${property}`,function(){return obj.property})
+//     }
+// }}
+//getting property
+let getsetGen = function ( obj) {
+for (let i in obj){
+    if(typeof obj[i] != 'function'){
+        obj["get" + i] = function(){
+            return this[i];
+        }
+        obj["set" + i] = function(){
+            this[i] = value;
+        }
+    };
+
+}
+
+return obj
+}
+
+
+
+console.log(getsetGen(rectangle))
+console.log(rectangle.getwidth())
+
